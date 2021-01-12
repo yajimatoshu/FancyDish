36f7b4746ea7e00fcbc1f6b8c2cd157110f2d74a
yajuma <yajuma>
2021-01-12 22:19:44 +0900

    first

---
+++ FancyDish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FancyDish
+//
+//  Created by SHUTO NISHIYAMA on 2021/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
